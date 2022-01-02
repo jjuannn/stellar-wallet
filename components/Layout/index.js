40c89e3ad5b8b60659c5194d1f3875688521766e
juan <juancraft12yt@gmail.com>
2022-01-02 17:17:49 -0300

    Update layout component

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import { Box, Flex } from "@chakra-ui/react";
 export default function AppLayout({ children }) {
   return (
     <Box as="section" display="flex" flexDirection="column" minHeight="100vh">
-      <Flex direction="column" as="main" flex="1">
+      <Flex padding={"5"} direction="column" as="main" flex="1">
         {children}
       </Flex>
       <footer>footer</footer>
