fc03746f6d5ca248f62e6de3d0354ebea81377dc
juan <juancraft12yt@gmail.com>
2022-01-02 17:17:58 -0300

    Update title component

--- components/Title/index.js
+++ components/Title/index.js
@@ -4,7 +4,7 @@ export default function Title({ title, subtitle }) {
   return (
     <>
       <Heading size={"2xl"}>{title}</Heading>
-      <Text fontSize={"2rem"} marginTop={"2"}>
+      <Text textAlign={"center"} fontSize={"2rem"} marginTop={"2"}>
         {subtitle}
       </Text>
     </>
